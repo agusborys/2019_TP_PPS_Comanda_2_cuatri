aec94ec5162220bfc08b0cb6dc783376e41dc4ec
agusborys <agus.borys@live.com.ar>
2019-11-19 02:35:25 -0300

    first commit

---
+++ glamit_app/src/app/clases/Anonimo.ts
@@ -0,0 +1,12 @@
+export class Anonimo {
+    public key: string;
+    public correo : string;
+    public nombre : string;
+    public foto : string;
+    constructor(){
+        this.key = "";
+        this.correo = "";
+        this.nombre = "";
+        this.foto = "";
+    }
+}
