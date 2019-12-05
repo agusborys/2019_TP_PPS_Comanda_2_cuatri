d4efc264248135837b194152f786211655afb166
LeonIfran <leonsifran@gmail.com>
2019-12-04 21:16:35 -0300

    implementado el toast para notifications en primer plano

--- comanda/src/app/app.component.ts
+++ comanda/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { timer } from 'rxjs/internal/observable/timer';
-
+import { ToastController } from '@ionic/angular';
 //import { timer } from 'rxjs/observable/timer';
 
 //FCM y Router se agregan para el funcionamiento de push notifications, lo mis con events
@@ -27,7 +27,8 @@ export class AppComponent implements OnInit {
     public splashScreen: SplashScreen,
     private fcm: FCM,
     public events: Events,
-    private router: Router
+    private router: Router,
+    public toastController: ToastController
   ) { }
 
   ngOnInit() {
@@ -52,8 +53,36 @@ export class AppComponent implements OnInit {
         this.router.navigateByUrl('/login');
       } else {
         console.log('Received in foreground');
-        this.router.navigateByUrl('/login');
+        /* this.router.navigateByUrl('/login'); */
+        this.presentToastWithOptions(data)
       }
     });
+  }
+
+  async presentToastWithOptions(mensaje: any) {
+    const toast = await this.toastController.create({
+      header: mensaje.notification.title,
+      message: mensaje.notification.body,
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Ir',
+          handler: () => {
+            console.log('Favorite clicked');
+            this.router.navigateByUrl('/list-confirmar-cliente-mesa');
+          }
+        }, {
+          text: 'Cerrar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+            toast.dismiss();
+          }
+        }
+      ]
+    });
+    toast.present();
   }
 }
